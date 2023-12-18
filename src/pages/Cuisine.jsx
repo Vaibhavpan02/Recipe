8a5c5bd79055bfdce6601cebe428db9a6092172f
Vaibhavpan02 <pandeyvaibhav513@gmail.com>
2023-12-18 19:25:21 +0530

    Recipe App Website

---
+++ src/pages/Cuisine.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom'; 
+
+function Cuisine() {
+  const [cuisine, setCuisine] = useState([]);
+  let params = useParams();
+
+  const getCuisine = async (name) => {
+    const apiKey = process.env.REACT_APP_API_KEY;
+    const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&number=16&cuisine=${name}`);
+    const recipes = await data.json();
+    setCuisine(recipes.results);
+  };
+
+  useEffect(() => {
+    getCuisine(params.type);
+  }, [params.type]);
+
+  return (
+    <Grid>
+      {cuisine.map((item) => {
+        return(
+        <Card key={item.id}>
+          <Link to={"/recipe/"+item.id}>
+          <img src={item.image} alt='' />
+          <h4>{item.title}</h4>
+          </Link>
+        </Card>
+        );
+        })}
+    </Grid>
+  );
+}
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(12rem, 1fr));
+  grid-gap: 2rem;
+`;
+
+const Card = styled.div`
+  img {
+    width: 100%;
+    border-radius: 2rem;
+  }
+
+  h4 {
+    text-align: center;
+    padding: 0rem;
+  }
+`;
+
+export default Cuisine;
